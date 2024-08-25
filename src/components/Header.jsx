bd59f033db4a12a5a7bdddde28a26f5cd96ad065
Sanjeev Kumar <109661686+softlabxsanjeev1@users.noreply.github.com>
2024-08-25 14:43:08 +0530

    Update resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
 
   const [actionButton, setActionButton] = useState({
     title: "Download CV",
-    link: 'https://drive.google.com/file/d/1bwObVC3emKEK-pSptaSLbFgrOAQ4VyH4/view?usp=drive_link'
+    link: 'https://drive.google.com/file/d/1MreEb44j5izeIsVx9Fl83gJW8LUQV9eL/view?usp=drive_link'
   });
 
   return (
